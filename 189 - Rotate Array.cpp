7cffa7f0cfd15c439a8f320754cb20d8206da1ef
Aman Gupta <aman.iv0012@gmail.com>
2021-11-20 09:50:42 +0530

    :bug: Resolved Rotate Array

--- 189 - Rotate Array.cpp
+++ 189 - Rotate Array.cpp
@@ -34,24 +34,11 @@ class Solution
 {
 public:
 	void rotate(vector<int> &nums, int k)
-	{
-		for (int i = 0; i < k; i++) //O(kn)
-		{
-			int f = nums[nums.size() - 1];
-			for (int j = nums.size() - 2; j >= 0; j--)
-			{
-				nums[j + 1] = nums[j];
-			}
-			nums[0] = f;
-		}
+	{ //lets say [1,2,3,4,5,6,7]
+		int n = nums.size();
+		k = k % n;
+		reverse(nums.begin(), nums.end() - k);		 // [4,3,2,1,5,6,7]
+		reverse(nums.begin() + (n - k), nums.end()); //[4,3,2,1,7,6,5]
+		reverse(nums.begin(), nums.end());			 // [5,6,7,1,2,3,4]
 	}
 };
-
-void rotate(vector<int> &nums, int k)
-{ //lets say [1,2,3,4,5,6,7]
-	int n = nums.size();
-	k = k % n;									 // If rotation is greater than array size
-	reverse(nums.begin(), nums.end() - k);		 // [4,3,2,1,5,6,7]
-	reverse(nums.begin() + (n - k), nums.end()); //[4,3,2,1,7,6,5]
-	reverse(nums.begin(), nums.end());			 // [5,6,7,1,2,3,4]
-}
