3d2d3793205e4eee404e0dc4d301f75a5e10be0c
Aman Gupta <amngupta@protonmail.com>
2022-07-19 15:08:55 +0530

    Random

---
+++ 1734. Decode XORed Permutation.cpp
@@ -0,0 +1,20 @@
+/*
+LC: https://leetcode.com/problems/decode-xored-permutation/
+*/
+class Solution {
+public:
+    vector<int> decode(vector<int>& A) {
+        int n = A.size() + 1, a = 0;
+        for (int i = 0; i <= n; ++i) {
+            a ^= i;
+            if (i < n && i % 2 == 1) {
+                a ^= A[i];
+            }
+        }
+        vector<int> res = {a};
+        for (int a: A) {
+            res.push_back(res.back() ^ a);
+        }
+        return res;
+    }
+};
