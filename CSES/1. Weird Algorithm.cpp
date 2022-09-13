a93b731d51d2919d34e28d42444728669a352c19
Aman Gupta <aman.iv0012@gmail.com>
2022-09-13 12:14:07 +0530

    Problems

--- CSES/1. Weird Algorithm.cpp
+++ CSES/1. Weird Algorithm.cpp
@@ -1,3 +1,7 @@
+/*
+https://cses.fi/problemset/task/1068
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Practice/Bit Manipulation/Copy Set Bits in Range.cpp
+++ Practice/Bit Manipulation/Copy Set Bits in Range.cpp
@@ -1,3 +1,43 @@
 /*
-
+https://practice.geeksforgeeks.org/problems/copy-set-bits-in-range0623/1
 */
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+	int setSetBit(int x, int y, int l, int r)
+	{
+		// code here
+		bitset<32> a(x), b(y);
+
+		for (int i = l - 1; i < r; i++)
+		{
+			if (b[i])
+				a[i] = 1;
+		}
+		return a.to_ulong();
+	}
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int x, y, l, r;
+		cin >> x >> y >> l >> r;
+
+		Solution ob;
+		cout << ob.setSetBit(x, y, l, r) << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
