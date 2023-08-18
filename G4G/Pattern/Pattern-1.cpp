da1d7d871477df8fb249b5db0bd3d4cd9b7f53fc
Aman Gupta <aman.iv0012@gmail.com>
2023-08-18 20:50:19 +0530

    snippets

---
+++ G4G/Pattern/Pattern-1.cpp
@@ -0,0 +1,16 @@
+// G4G:
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(int n, int m)
+{
+	
+}
+
+// Driver Code
+int main()
+{
+	int rows = 6, columns = 20;
+	solution(rows, columns);
+	return 0;
+}
