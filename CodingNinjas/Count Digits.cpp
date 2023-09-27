05fad0c844df6c4f372db2c8fdb6e94613740d9f
Aman Gupta <aman.iv0012@gmail.com>
2023-09-27 16:10:36 +0000

    Create Count Digits.cpp

---
+++ CodingNinjas/Count Digits.cpp
@@ -0,0 +1,12 @@
+CN: https://www.codingninjas.com/studio/problems/count-digits_8416387
+
+int countDigits(int n){
+	int res=0;
+	int a=n;
+	while(a){
+		if(a%10!=0 && n%(a%10)==0)
+			res++;
+		a/=10;
+	}
+	return res;
+}
