e0c107c246bd5d3b25e562c9ae2b496e0bbd27d0
Aman Gupta <aman.iv0012@gmail.com>
2023-09-27 16:36:27 +0000

    Create Check Armstrong.cpp

---
+++ CodingNinjas/Check Armstrong.cpp
@@ -0,0 +1,13 @@
+CN: https://www.codingninjas.com/studio/problems/check-armstrong_589
+
+#include <math.h>
+bool checkArmstrong(int n){
+	int a=n, arm=0, d=to_string(n).size();
+	while(n){
+		arm = arm + (int)pow(n%10,d);
+		n/=10;
+	}
+	if(a == arm)
+		return true;
+	return false;
+}
